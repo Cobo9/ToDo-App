a9c2b5f5e7def707a5061c7293e4c695a4c2d1a6
Jakub Gond <jakubgond@gmail.com>
2020-12-05 12:53:39 +0100

    commit with bug after editing

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ var tasks = [];
 let taskId= 0;
 const todoForm = document.querySelector(".todolist__form");
 const todoInput = document.querySelector(".todolist__form__input");
+const todoButton = document.querySelector(".todolist__form__button");
 const todoList = document.querySelector(".todolist__list");
 todoForm.addEventListener("submit", submitForm);
+
+
 
 function submitForm (e){
     e.preventDefault();
@@ -20,7 +23,6 @@ function addTask (text){
     }
     tasks.push(task);
     renderTodo();
-
 }
 
 function removeItem(id){
@@ -31,8 +33,49 @@ function removeItem(id){
 }
 
 
+
+function editItem(id){
+
+    // First we need to find the element in the array of tasks
+    let tasksArray = [...tasks];
+    const elementToEdit = tasksArray.find(item=> item.id == id.dataset.key)
+
+    //prepare the input field and button, input field takes the value of the text on the item in the array.
+    todoInput.value = elementToEdit.name;
+    todoButton.textContent="Edit";
+
+    //now the user can edit their task in the input field
+    //we need to remove the submit event listener from the form, and add a different 
+    //one to fetch the input value and change the textContent of task;
+    todoForm.removeEventListener("submit", submitForm);
+    todoForm.addEventListener("submit", (e)=>fetchValue(e,todoInput.value, elementToEdit));
+
+
+}
+
+
+
+function fetchValue (e,todoInputValue, elementToEdit){
+    ///first we block the refresher
+    e.preventDefault()
+    //second we alter the name of the element in the array to the one that user wrote
+    elementToEdit.name = todoInputValue;
+
+    //restore the button and the input field to default behavior
+    todoInput.value = "";
+    todoButton.textContent="Submit";
+
+    //we need to restore the listeners to their default behaviour
+    todoForm.removeEventListener("submit", (e)=>fetchValue(e,todoInput.value, elementToEdit));
+    todoForm.addEventListener("submit", submitForm);
+    //render of the newly altered list
+    renderTodo();
+}
+
+
 function renderTodo(){
     todoList.innerHTML="";
+
     tasks.forEach(item=> {
 
         const input = document.createElement("input");
@@ -45,6 +88,7 @@ function renderTodo(){
         const editBtn = document.createElement("button")
         editBtn.classList.add("todolist__list__item__button");
         editBtn.classList.add("todolist__list__item__button--edit");
+        editBtn.setAttribute("data-key", `${item.id}`);
         editBtn.appendChild(editIcon)
 
         const deleteIcon = document.createElement("i")
@@ -75,6 +119,9 @@ function renderTodo(){
 
     const removeBtns = document.querySelectorAll(".todolist__list__item__button--delete");
     removeBtns.forEach(item => item.addEventListener("click", ()=>{removeItem(item)}));
+
+    const editBtns = document.querySelectorAll(".todolist__list__item__button--edit");
+    editBtns.forEach(item => item.addEventListener("click", ()=>{editItem(item)}));
 }
 
 
